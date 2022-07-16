7145c92d1c17e3991ea5a6061733a1f463595a2e
Mubarak Mayyeri <mubarakmayyeri@gmail.com>
2022-07-16 13:09:59 +0530

    added digit buttons and label

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import tkinter as tk
 
+SMALL_FONT_STYLE = ("Ariel", 16)
+LARGE_FONT_STYLE = ("Ariel", 40, "bold")
+DIGIT_FONT_STYLE = ("Ariel", 24, "bold")
+WHITE = "#FFFFFF"
+LIGHT_GRAY = "#F5F5F5"
+LABEL_COLOR = "#25265E"
+
 
 class Calculator:
     def __init__(self):
@@ -7,6 +14,47 @@ class Calculator:
         self.window.geometry("375x567")
         self.window.resizable(0, 0)
         self.window.title("Calculator")
+
+        self.total_expression = "0"
+        self.current_expression = "0"
+        self.display_frame = self.create_display_frame()
+
+        self.total_label, self.label = self.create_display_labels()
+
+        self.digits = {
+            7: (1, 1), 8: (1, 2), 9: (1, 3),
+            4: (2, 1), 5: (2, 2), 6: (2, 3),
+            1: (3, 1), 2: (3, 2), 3: (3, 3),
+            '.': (4, 1), 0: (4, 2)
+        }
+        self.button_frame = self.create_button_frame()
+        self.create_digit_buttons()
+
+    def create_display_labels(self):
+        total_label = tk.Label(self.display_frame, text=self.total_expression, anchor=tk.E, bg=LIGHT_GRAY,
+                               fg=LABEL_COLOR, padx=24, font=SMALL_FONT_STYLE)
+        total_label.pack(expand=True, fill="both")
+
+        label = tk.Label(self.display_frame, text=self.current_expression, anchor=tk.E, bg=LIGHT_GRAY,
+                         fg=LABEL_COLOR, padx=24, font=LARGE_FONT_STYLE)
+        label.pack(expand=True, fill="both")
+
+        return total_label, label
+
+    def create_display_frame(self):
+        frame = tk.Frame(self.window, height=221, bg=LIGHT_GRAY)
+        frame.pack(expand=True, fill="both")
+        return frame
+
+    def create_digit_buttons(self):
+        for digit,grid_value in self.digits.items():
+            button = tk.Button(self.button_frame, text=str(digit), bg=WHITE, fg=LABEL_COLOR, font=DIGIT_FONT_STYLE, borderwidth=0)
+            button.grid(row=grid_value[0], column=grid_value[1], sticky=tk.NSEW)
+
+    def create_button_frame(self):
+        frame = tk.Frame(self.window)
+        frame.pack(expand=True, fill="both")
+        return frame
 
     def run(self):
         self.window.mainloop()
